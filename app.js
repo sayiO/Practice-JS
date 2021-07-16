78c7d1f893200d1088e54c09daf82465ac287d08
sayiO <sayionias@gmail.com>
2021-07-17 01:43:17 +0200

    demonstrated the slice() method

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-let arr = ["alpha", "gamma", "delta"];
+let arr = ["alpha", "gamma", "delta", "simbie", "tanaka", "rachie"];
+arr2 = arr.slice();
 
 function myFunction() {
-    document.getElementById("demo").innerHTML = arr.join("-");
+    document.getElementById("demo").innerHTML = arr2;
+
+}
+
+function myFunction1() {
+    document.getElementById("demo").innerHTML = arr.slice(2, 5);
 
 }

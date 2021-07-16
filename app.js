665d62be3987e80b68ebf471aafd688bbcf2b745
sayiO <sayionias@gmail.com>
2021-07-17 01:32:35 +0200

    demostrated the array join() method

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-let arr = [2, 3, 4, 5, "bee", "tinoe", "simbie"];
-console.log(arr.splice[1, 1]);
+let arr = ["alpha", "gamma", "delta"];
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = arr.join("-");
+
+}

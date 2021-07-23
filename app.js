6be58c8f35538062b413df98dde1dcdc76e978c3
sayiO <sayionias@gmail.com>
2021-07-23 23:58:54 +0200

    function counting multiple letters in a string producing an object

--- app.js
+++ app.js
@@ -40,8 +40,17 @@ function createCal() {
     }
 }
 
-
-
+function multipleLetterCount(str) {
+    let finalObj = {};
+    for (let i = 0; i < str.length; i++) {
+        if (!(str[i] in finalObj)) {
+            finalObj[str[i]] = 1;
+        } else {
+            finalObj[str[i]]++;
+        }
+    }
+    return finalObj;
+}
 
 
 

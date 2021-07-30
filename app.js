dd799d38179e230a5e14cdf87437f2e93a236646
sayiO <sayionias@gmail.com>
2021-07-30 12:06:54 +0200

    solved the multi-dimensional array problem

--- app.js
+++ app.js
@@ -246,3 +246,16 @@ function myFunction7() {
 
 
 }
+
+function multiDimenArrays() {
+    let nestedArr = [
+        [1, 2, 3],
+        [4, 5, 6, 7],
+        [8, 9, 10, 11]
+    ];
+    for (let i = 0; i < nestedArr.length; i++) {
+        for (let j = 0; j < nestedArr[i]; j++) {
+            console.log(nestedArr[i][j]);
+        }
+    }
+}
